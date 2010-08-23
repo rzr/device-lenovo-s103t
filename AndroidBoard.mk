a4a52c3cacdc4b58c6a26ca7d73e13376aff4552
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-08-23 15:01:35 +0800

    initial import for froyo-x86

---
+++ AndroidBoard.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+LOCAL_FIRMWARES := RT2860STA/RT2860STA.dat
+TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
+TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
+TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
+
+include $(GENERIC_X86_ANDROID_MK)
---
+++ eeepc.mk
@@ -0,0 +1,6 @@
+PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
+
+$(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
+
+PRODUCT_NAME := eeepc
+PRODUCT_DEVICE := eeepc
