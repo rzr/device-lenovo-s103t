f33ff64c6701cf6d76e52ec486088606ee36c5e7
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-09-06 18:55:33 +0800

    add wifi support
    Use AWEXT provided by Nicu Pavel.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := RT2860STA/RT2860STA.dat
+LOCAL_FIRMWARES := rt2860.bin
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
