fd25666c8a8381d7ad8c9cea79f39de1f63fb896
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-01-24 00:17:22 +0800

    update for gingerbread-x86

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_FIRMWARES := rt2860.bin
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
-TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
 
 include $(GENERIC_X86_ANDROID_MK)
