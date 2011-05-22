99453e79b6c853bfca5ead5e2519f7199c17f7ff
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-05-22 17:48:59 +0800

    support Eee PC 1215T
    * Enable AMD CPU support
    * Add firmware for the radeon driver
    * Enable hardware acceleration of R600G

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := rt2860.bin
+LOCAL_FIRMWARES := rt2860.bin radeon/R600_rlc.bin
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
