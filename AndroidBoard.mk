bde563923d920d18fdd6a955de8b662c8c892c8d
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-08-08 12:55:15 +0800

    update for kernel 2.6.39 and new mesa library

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := rt2860.bin radeon/R600_rlc.bin
+LOCAL_FIRMWARES := rt2860.bin $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/*))
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
 
